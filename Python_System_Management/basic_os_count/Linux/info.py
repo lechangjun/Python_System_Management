90f942194be1862910d31db0fc557114769c6250
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-07-26 17:58:52 -0700

    Update info.py

--- Python_System_Management/basic_os_count/Linux/info.py
+++ Python_System_Management/basic_os_count/Linux/info.py
@@ -4,8 +4,13 @@
 import platform
 import multiprocessing
 
-print "운영체제: ", platform.system()
-print "운영체제의 상세정보: ", platform.platform()
-print "운영체제 버전: ", platform.version()
-print "프로세서: ", platform.processor()
-print "CPU 수: ", multiprocessing.cpu_count()
+# print "운영체제: ", platform.system()
+platform.system()
+#print "운영체제의 상세정보: ", 
+platform.platform()
+#print "운영체제 버전: ", 
+platform.version()
+#print "프로세서: ", 
+platform.processor()
+#print "CPU 수: ", 
+multiprocessing.cpu_count()
